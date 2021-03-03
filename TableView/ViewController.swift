352a3d07aebffddee47937de71ebc0b424949879
JuanJCO <jcarroolmedo@gmail.com>
2021-03-03 02:29:49 +0100

    final commit

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let tableView: UITableView = {
+        let tv = UITableView()
+        tv.backgroundColor = UIColor.white
+        tv.separatorColor = UIColor.white
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupTableView()
+    }
+    
+    func setupTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(ThirtyDayCell.self, forCellReuseIdentifier: "cellID")
+        
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor)
+        ])
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath) as! ThirtyDayCell
+        cell.backgroundColor = UIColor.white
+        cell.dayLabel.text = "Day \(indexPath.row*1)"
+        
+        return cell
     }
 
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    
+    
 
 }
 
